91ff9f49af49be155d391edcc5875286ed2a703a
gullz <bpulathisi@hotmail.com>
2015-06-18 13:47:31 +0530

    Initial Commit

---
+++ LVQ/Form1.vb
@@ -0,0 +1,298 @@
+﻿Public Class Form1
+    Private Const NUMBER_OF_CLUSTERS As Integer = 7
+    Private Const VEC_LEN As Integer = 63
+    Private Const TRAINING_PATTERNS As Integer = 21
+
+    Private LVQ As LVQ1class
+
+    'If you look closely, you can see the letters made out of 1's
+    Private A1 As Integer() = New Integer() {0, 0, 1, 1, 0, 0, 0, _
+                                             0, 0, 0, 1, 0, 0, 0, _
+                                             0, 0, 0, 1, 0, 0, 0, _
+                                             0, 0, 1, 0, 1, 0, 0, _
+                                             0, 0, 1, 0, 1, 0, 0, _
+                                             0, 1, 1, 1, 1, 1, 0, _
+                                             0, 1, 0, 0, 0, 1, 0, _
+                                             0, 1, 0, 0, 0, 1, 0, _
+                                             1, 1, 1, 0, 1, 1, 1}
+
+    Private A As Integer() = New Integer() {1, 1, 1, 0, 0, 1, 1, _
+                                             0, 1, 0, 0, 0, 1, 0, _
+                                             0, 1, 0, 0, 1, 0, 0, _
+                                             0, 1, 0, 1, 0, 0, 0, _
+                                             0, 1, 1, 0, 0, 0, 0, _
+                                             0, 1, 0, 1, 0, 0, 0, _
+                                             0, 1, 0, 0, 1, 0, 0, _
+                                             0, 1, 0, 0, 0, 1, 0, _
+                                             1, 1, 1, 0, 0, 1, 1}
+
+    Private B1 As Integer() = New Integer() {1, 1, 1, 1, 1, 1, 0, _
+                                             0, 1, 0, 0, 0, 0, 1, _
+                                             0, 1, 0, 0, 0, 0, 1, _
+                                             0, 1, 0, 0, 0, 0, 1, _
+                                             0, 1, 1, 1, 1, 1, 0, _
+                                             0, 1, 0, 0, 0, 0, 1, _
+                                             0, 1, 0, 0, 0, 0, 1, _
+                                             0, 1, 0, 0, 0, 0, 1, _
+                                             1, 1, 1, 1, 1, 1, 0}
+
+    Private C1 As Integer() = New Integer() {0, 0, 1, 1, 1, 1, 1, _
+                                             0, 1, 0, 0, 0, 0, 1, _
+                                             1, 0, 0, 0, 0, 0, 0, _
+                                             1, 0, 0, 0, 0, 0, 0, _
+                                             1, 0, 0, 0, 0, 0, 0, _
+                                             1, 0, 0, 0, 0, 0, 0, _
+                                             1, 0, 0, 0, 0, 0, 0, _
+                                             0, 1, 0, 0, 0, 0, 1, _
+                                             0, 0, 1, 1, 1, 1, 0}
+
+    Private D1 As Integer() = New Integer() {1, 1, 1, 1, 1, 0, 0, _
+                                             0, 1, 0, 0, 0, 1, 0, _
+                                             0, 1, 0, 0, 0, 0, 1, _
+                                             0, 1, 0, 0, 0, 0, 1, _
+                                             0, 1, 0, 0, 0, 0, 1, _
+                                             0, 1, 0, 0, 0, 0, 1, _
+                                             0, 1, 0, 0, 0, 0, 1, _
+                                             0, 1, 0, 0, 0, 1, 0, _
+                                             1, 1, 1, 1, 1, 0, 0}
+
+    Private E1 As Integer() = New Integer() {1, 1, 1, 1, 1, 1, 1, _
+                                             0, 1, 0, 0, 0, 0, 1, _
+                                             0, 1, 0, 0, 0, 0, 0, _
+                                             0, 1, 0, 1, 0, 0, 0, _
+                                             0, 1, 1, 1, 0, 0, 0, _
+                                             0, 1, 0, 1, 0, 0, 0, _
+                                             0, 1, 0, 0, 0, 0, 0, _
+                                             0, 1, 0, 0, 0, 0, 1, _
+                                             1, 1, 1, 1, 1, 1, 1}
+
+    Private J1 As Integer() = New Integer() {0, 0, 0, 1, 1, 1, 1, _
+                                             0, 0, 0, 0, 0, 1, 0, _
+                                             0, 0, 0, 0, 0, 1, 0, _
+                                             0, 0, 0, 0, 0, 1, 0, _
+                                             0, 0, 0, 0, 0, 1, 0, _
+                                             0, 0, 0, 0, 0, 1, 0, _
+                                             0, 1, 0, 0, 0, 1, 0, _
+                                             0, 1, 0, 0, 0, 1, 0, _
+                                             0, 0, 1, 1, 1, 0, 0}
+
+    Private K1 As Integer() = New Integer() {1, 1, 1, 0, 0, 1, 1, _
+                                             0, 1, 0, 0, 1, 0, 0, _
+                                             0, 1, 0, 1, 0, 0, 0, _
+                                             0, 1, 1, 0, 0, 0, 0, _
+                                             0, 1, 1, 0, 0, 0, 0, _
+                                             0, 1, 0, 1, 0, 0, 0, _
+                                             0, 1, 0, 0, 1, 0, 0, _
+                                             0, 1, 0, 0, 0, 1, 0, _
+                                             1, 1, 1, 0, 0, 1, 1}
+
+    Private A2 As Integer() = New Integer() {0, 0, 0, 1, 0, 0, 0, _
+                                             0, 0, 0, 1, 0, 0, 0, _
+                                             0, 0, 0, 1, 0, 0, 0, _
+                                             0, 0, 1, 0, 1, 0, 0, _
+                                             0, 0, 1, 0, 1, 0, 0, _
+                                             0, 1, 0, 0, 0, 1, 0, _
+                                             0, 1, 1, 1, 1, 1, 0, _
+                                             0, 1, 0, 0, 0, 1, 0, _
+                                             0, 1, 0, 0, 0, 1, 0}
+
+    Private B2 As Integer() = New Integer() {1, 1, 1, 1, 1, 1, 0, _
+                                             1, 0, 0, 0, 0, 0, 1, _
+                                             1, 0, 0, 0, 0, 0, 1, _
+                                             1, 0, 0, 0, 0, 0, 1, _
+                                             1, 1, 1, 1, 1, 1, 0, _
+                                             1, 0, 0, 0, 0, 0, 1, _
+                                             1, 0, 0, 0, 0, 0, 1, _
+                                             1, 0, 0, 0, 0, 0, 1, _
+                                             1, 1, 1, 1, 1, 1, 0}
+
+    Private C2 As Integer() = New Integer() {0, 0, 1, 1, 1, 0, 0, _
+                                             0, 1, 0, 0, 0, 1, 0, _
+                                             1, 0, 0, 0, 0, 0, 1, _
+                                             1, 0, 0, 0, 0, 0, 0, _
+                                             1, 0, 0, 0, 0, 0, 0, _
+                                             1, 0, 0, 0, 0, 0, 0, _
+                                             1, 0, 0, 0, 0, 0, 1, _
+                                             0, 1, 0, 0, 0, 1, 0, _
+                                             0, 0, 1, 1, 1, 0, 0}
+
+    Private D2 As Integer() = New Integer() {1, 1, 1, 1, 1, 0, 0, _
+                                             1, 0, 0, 0, 0, 1, 0, _
+                                             1, 0, 0, 0, 0, 0, 1, _
+                                             1, 0, 0, 0, 0, 0, 1, _
+                                             1, 0, 0, 0, 0, 0, 1, _
+                                             1, 0, 0, 0, 0, 0, 1, _
+                                             1, 0, 0, 0, 0, 0, 1, _
+                                             1, 0, 0, 0, 0, 1, 0, _
+                                             1, 1, 1, 1, 1, 0, 0}
+
+    Private E2 As Integer() = New Integer() {1, 1, 1, 1, 1, 1, 1, _
+                                             1, 0, 0, 0, 0, 0, 0, _
+                                             1, 0, 0, 0, 0, 0, 0, _
+                                             1, 0, 0, 0, 0, 0, 0, _
+                                             1, 1, 1, 1, 1, 0, 0, _
+                                             1, 0, 0, 0, 0, 0, 0, _
+                                             1, 0, 0, 0, 0, 0, 0, _
+                                             1, 0, 0, 0, 0, 0, 0, _
+                                             1, 1, 1, 1, 1, 1, 1}
+
+    Private J2 As Integer() = New Integer() {0, 0, 0, 0, 0, 1, 0, _
+                                             0, 0, 0, 0, 0, 1, 0, _
+                                             0, 0, 0, 0, 0, 1, 0, _
+                                             0, 0, 0, 0, 0, 1, 0, _
+                                             0, 0, 0, 0, 0, 1, 0, _
+                                             0, 0, 0, 0, 0, 1, 0, _
+                                             0, 1, 0, 0, 0, 1, 0, _
+                                             0, 1, 0, 0, 0, 1, 0, _
+                                             0, 0, 1, 1, 1, 0, 0}
+
+    Private K2 As Integer() = New Integer() {1, 0, 0, 0, 0, 1, 0, _
+                                             1, 0, 0, 0, 1, 0, 0, _
+                                             1, 0, 0, 1, 0, 0, 0, _
+                                             1, 0, 1, 0, 0, 0, 0, _
+                                             1, 1, 0, 0, 0, 0, 0, _
+                                             1, 0, 1, 0, 0, 0, 0, _
+                                             1, 0, 0, 1, 0, 0, 0, _
+                                             1, 0, 0, 0, 1, 0, 0, _
+                                             1, 0, 0, 0, 0, 1, 0}
+
+    Private A3 As Integer() = New Integer() {0, 0, 0, 1, 0, 0, 0, _
+                                             0, 0, 0, 1, 0, 0, 0, _
+                                             0, 0, 1, 0, 1, 0, 0, _
+                                             0, 0, 1, 0, 1, 0, 0, _
+                                             0, 1, 0, 0, 0, 1, 0, _
+                                             0, 1, 1, 1, 1, 1, 0, _
+                                             1, 0, 0, 0, 0, 0, 1, _
+                                             1, 0, 0, 0, 0, 0, 1, _
+                                             1, 1, 0, 0, 0, 1, 1}
+
+    Private B3 As Integer() = New Integer() {1, 1, 1, 1, 1, 1, 0, _
+                                             0, 1, 0, 0, 0, 0, 1, _
+                                             0, 1, 0, 0, 0, 0, 1, _
+                                             0, 1, 1, 1, 1, 1, 0, _
+                                             0, 1, 0, 0, 0, 0, 1, _
+                                             0, 1, 0, 0, 0, 0, 1, _
+                                             0, 1, 0, 0, 0, 0, 1, _
+                                             0, 1, 0, 0, 0, 0, 1, _
+                                             1, 1, 1, 1, 1, 1, 0}
+
+    Private C3 As Integer() = New Integer() {0, 0, 1, 1, 1, 0, 1, _
+                                             0, 1, 0, 0, 0, 1, 1, _
+                                             1, 0, 0, 0, 0, 0, 1, _
+                                             1, 0, 0, 0, 0, 0, 0, _
+                                             1, 0, 0, 0, 0, 0, 0, _
+                                             1, 0, 0, 0, 0, 0, 0, _
+                                             1, 0, 0, 0, 0, 0, 1, _
+                                             0, 1, 0, 0, 0, 1, 0, _
+                                             0, 0, 1, 1, 1, 0, 0}
+
+    Private D3 As Integer() = New Integer() {1, 1, 1, 1, 0, 0, 0, _
+                                             0, 1, 0, 0, 1, 0, 0, _
+                                             0, 1, 0, 0, 0, 1, 0, _
+                                             0, 1, 0, 0, 0, 1, 0, _
+                                             0, 1, 0, 0, 0, 1, 0, _
+                                             0, 1, 0, 0, 0, 1, 0, _
+                                             0, 1, 0, 0, 0, 1, 0, _
+                                             0, 1, 0, 0, 1, 0, 0, _
+                                             1, 1, 1, 1, 0, 0, 0}
+
+    Private E3 As Integer() = New Integer() {1, 1, 1, 1, 1, 1, 1, _
+                                             0, 1, 0, 0, 0, 0, 1, _
+                                             0, 1, 0, 0, 1, 0, 0, _
+                                             0, 1, 1, 1, 1, 0, 0, _
+                                             0, 1, 0, 0, 1, 0, 0, _
+                                             0, 1, 0, 0, 0, 0, 0, _
+                                             0, 1, 0, 0, 0, 0, 0, _
+                                             0, 1, 0, 0, 0, 0, 1, _
+                                             1, 1, 1, 1, 1, 1, 1}
+
+    Private J3 As Integer() = New Integer() {0, 0, 0, 0, 1, 1, 1, _
+                                             0, 0, 0, 0, 0, 1, 0, _
+                                             0, 0, 0, 0, 0, 1, 0, _
+                                             0, 0, 0, 0, 0, 1, 0, _
+                                             0, 0, 0, 0, 0, 1, 0, _
+                                             0, 0, 0, 0, 0, 1, 0, _
+                                             0, 0, 0, 0, 0, 1, 0, _
+                                             0, 1, 0, 0, 0, 1, 0, _
+                                             0, 0, 1, 1, 1, 0, 0}
+
+    Private K3 As Integer() = New Integer() {1, 1, 1, 0, 0, 1, 1, _
+                                             0, 1, 0, 0, 0, 1, 0, _
+                                             0, 1, 0, 0, 1, 0, 0, _
+                                             0, 1, 0, 1, 0, 0, 0, _
+                                             0, 1, 1, 0, 0, 0, 0, _
+                                             0, 1, 0, 1, 0, 0, 0, _
+                                             0, 1, 0, 0, 1, 0, 0, _
+                                             0, 1, 0, 0, 0, 1, 0, _
+                                             1, 1, 1, 0, 0, 1, 1}
+
+    Private mFontNames As String() = New String() {"A1", "B1", "C1", "D1", _
+                                                    "E1", "J1", "K1", _
+                                                    "A2", "B2", "C2", "D2", _
+                                                    "E2", "J2", "K2", _
+                                                    "A3", "B3", "C3", "D3", _
+                                                    "E3", "J3", "K3"}
+
+    Private Pattern As Integer()() = New Integer(20)() {}
+
+    Private Target As Integer() = New Integer() {0, 1, 2, 3, 4, 5, 6, _
+                                                 0, 1, 2, 3, 4, 5, 6, _
+                                                 0, 1, 2, 3, 4, 5, 6}
+
+    Private Sub InitializePatterns()
+        'Insert pattern arrays into Pattern() to make an array of arrays.
+        Pattern(0) = A1
+        Pattern(1) = B1
+        Pattern(2) = C1
+        Pattern(3) = D1
+        Pattern(4) = E1
+        Pattern(5) = J1
+        Pattern(6) = K1
+        Pattern(7) = A2
+        Pattern(8) = B2
+        Pattern(9) = C2
+        Pattern(10) = D2
+        Pattern(11) = E2
+        Pattern(12) = J2
+        Pattern(13) = K2
+        Pattern(14) = A3
+        Pattern(15) = B3
+        Pattern(16) = C3
+        Pattern(17) = D3
+        Pattern(18) = E3
+        Pattern(19) = J3
+        Pattern(20) = K3
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As Object, ByVal e As System.EventArgs) _
+                                                                Handles Button1.Click
+        Dim i As Integer
+
+        InitializePatterns()
+        LVQ = New LVQ1class(NUMBER_OF_CLUSTERS, _
+                            VEC_LEN, TRAINING_PATTERNS, _
+                            Pattern, _
+                            Target)
+
+        For i = 0 To NUMBER_OF_CLUSTERS - 1
+            LVQ.initializeWeights(i, Pattern(i))
+            TextBox1.Text += "Weights for cluster " + CStr(i) + _
+                          " initialized to pattern " + CStr(mFontNames(i)) + _
+                          vbCrLf
+        Next i
+
+        LVQ.trainNetwork()
+        i = LVQ.getCluster(A)
+        TextBox1.Text = "Pattern " + mFontNames(i) + " belongs to cluster " + _
+                       CStr(i) + vbCrLf
+        'For i = 0 To 6
+        '    For j = 0 To 20
+        '        Debug.Print(LVQ.weights(i, j).ToString)
+        '    Next j
+        'Next i
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ LVQ/LVQ1class.vb
@@ -0,0 +1,224 @@
+﻿Public Class LVQ1class
+
+    Private Const DECAY_RATE As Double = 0.96 'About 100 iterations. 
+    Private Const MIN_ALPHA As Double = 0.01
+    Private Const RADIUS_REDUCTION_POINT As Double = 0.023 'Last 20% of iterations.
+
+
+    Private clusters As Integer 'Total number of clusters.
+    Private vLength As Integer 'Vector length.
+    Private mIterations As Integer
+    Private mReductionPoint As Integer = 0
+    Private Alpha As Double = 0.6
+    Private D() As Double 'Network nodes. The "clusters"
+
+    'Weight matrix.
+    Private w(,) As Double
+
+    'Training patterns.
+    Private mTrainPatterns
+    Private mPattern As Integer()()
+    Private mTarget As Integer()
+
+    Public Sub New(ByVal numClusters As Integer, _
+                   ByVal vecLength As Integer, _
+                   ByVal numTPatterns As Integer, _
+                   ByVal tPatterns As Integer()(), _
+                   ByVal targets As Integer())
+
+        clusters = numClusters
+        vLength = vecLength
+        mTrainPatterns = numTPatterns
+        ReDim D(clusters - 1) 'Network nodes.
+
+        'Training patterns.
+        mPattern = New Integer(numTPatterns - 1)() {}
+        mPattern = tPatterns
+
+        ReDim mTarget(mTrainPatterns - 1)
+        mTarget = targets
+
+        'Weight matrix to be filled with values between 0.0 and 1.0
+        ReDim w(clusters - 1, vLength - 1)
+
+    End Sub
+
+    Public Sub trainNetwork()
+
+        training()
+
+    End Sub
+
+    Public Sub initializeWeights(ByVal clusterNumber As Integer, _
+                                 ByVal trainingPattern As Integer())
+        'clusterNumber = the output node (cluster) to assign the pattern to.
+        'trainingPattern = the pattern which the output node will respond to.
+        Dim i As Integer
+
+        'Initialize weights.
+        For i = 0 To vLength - 1
+            w(clusterNumber, i) = CDbl(trainingPattern(i))
+        Next i
+
+    End Sub
+
+    Private Sub training()
+        Dim ReductionFlag As Boolean = False
+        Dim VecNum As Integer
+        Dim DMin As Integer
+
+        mIterations = 0
+
+        While Alpha > MIN_ALPHA
+            mIterations += 1
+
+            For VecNum = 0 To mTrainPatterns - 1
+
+                'Compute input for all nodes.
+                ComputeInput(mPattern, VecNum)
+
+                'See which is smaller?
+                DMin = minimum(D)
+                Debug.Print(DMin)
+                'Update the weights on the winning unit.
+                UpdateWeights(VecNum, DMin)
+
+            Next VecNum
+
+            'Reduce the learning rate.
+            Alpha = DECAY_RATE * Alpha
+
+            'Reduce radius at specified point.
+            If Alpha < RADIUS_REDUCTION_POINT Then
+                If ReductionFlag = False Then
+                    ReductionFlag = True
+                    mReductionPoint = mIterations
+                End If
+            End If
+
+        End While
+
+    End Sub
+
+    Public ReadOnly Property trainingPatterns()
+        Get
+            Return mTrainPatterns
+        End Get
+    End Property
+
+    Public ReadOnly Property iterations()
+        Get
+            Return mIterations
+        End Get
+    End Property
+
+    Public ReadOnly Property reductionPoint()
+        Get
+            Return mReductionPoint
+        End Get
+    End Property
+
+    Public ReadOnly Property patterns() As Integer()()
+        Get
+            Return mPattern
+        End Get
+    End Property
+
+    Public ReadOnly Property weights() As Double(,)
+        Get
+            Return w
+        End Get
+    End Property
+
+    Public Function getCluster(ByVal inputPattern As Integer()) As Integer
+
+        'Compute input for all nodes.
+        ComputeInput(inputPattern)
+
+        'See which is smaller?
+        Return minimum(D)
+
+    End Function
+
+    Private Sub UpdateWeights(ByVal VectorNumber As Integer, ByVal DMin As Integer)
+        Dim i As Integer
+
+        For i = 0 To vLength - 1
+            'Update the winner.
+            If DMin = mTarget(VectorNumber) Then
+                w(DMin, i) += (Alpha * (mPattern(VectorNumber)(i) - w(DMin, i)))
+            Else
+                w(DMin, i) -= (Alpha * (mPattern(VectorNumber)(i) - w(DMin, i)))
+            End If
+        Next i
+
+    End Sub
+
+    Private Sub ComputeInput(ByRef VectorArray As Integer()(), _
+                             ByVal VectorNumber As Integer)
+        'Overloaded function.  See ComputeInput below.
+        Dim i, j As Integer
+
+        clearArray(D)
+        For i = 0 To clusters - 1
+            For j = 0 To vLength - 1
+                D(i) += (w(i, j) - VectorArray(VectorNumber)(j)) ^ 2
+
+            Next j
+        Next i
+
+    End Sub
+
+    Private Sub ComputeInput(ByRef VectorArray As Integer())
+        'Overloaded function.  See ComputeInput above.
+        Dim i, j As Integer
+
+        clearArray(D)
+        For i = 0 To clusters - 1
+            For j = 0 To vLength - 1
+                D(i) += (w(i, j) - VectorArray(j)) ^ 2
+                'Debug.Print(D(i))
+            Next j
+        Next i
+
+    End Sub
+
+    Private Sub clearArray(ByRef anArray As Double())
+        Dim i As Integer
+
+        For i = 0 To clusters - 1
+            anArray(i) = 0.0
+        Next i
+
+    End Sub
+
+    Private Function minimum(ByRef NodeArray As Double()) As Integer
+        Dim i As Integer
+        Dim Winner As Integer
+        Dim FoundNewWinner As Boolean
+        Dim Done As Boolean = False
+
+        Winner = 0
+        Do Until Done
+
+            FoundNewWinner = False
+            For i = 0 To clusters - 1
+                If i <> Winner Then             'Avoid self-comparison.
+                    If NodeArray(i) < NodeArray(Winner) Then
+                        Winner = i
+                        FoundNewWinner = True
+                    End If
+                End If
+            Next i
+
+            If FoundNewWinner = False Then
+                Done = True
+            End If
+
+        Loop
+
+        Return Winner
+
+    End Function
+
+End Class
